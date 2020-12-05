7035f89c1e57a3311de40b550263d0999ab79f42
MisterPython581 <70380949+MisterPython581@users.noreply.github.com>
2020-12-05 18:49:28 +0100

    Performance update

--- languagemanager.py
+++ languagemanager.py
@@ -18,17 +18,18 @@ from language_manager.info.settings import SETTINGS as lm_settings
 
 
 class LanguageManager:
+    def __init__(self):
+        super(LanguageManager, self).__init__()
+        self.language_name_global = ""
+        self.key_global = ""
+
     def get_key(self, language_name, key):
         """ Get a key in a language package """
+        self.language_name_global = language_name
+        self.key_global = key
         package_path = f"language_manager/packages/{language_name}.langpkg"
-        with open(package_path, "r") as file:
-            with open(f"temp.py", "w") as temp_file:
-                # Log ONLY IF the "debug" value in the settings == True
-                self.__log__(f"--- Call to LanguageManager.get_key(self, {language_name}, {key}) ---\n"
-                             f"Copying file {package_path} to LanguageManager root/temp.py\n")
-                # Copy the language package into the file temp.py in the root of LanguageManager
-                temp_file.write(file.read())
-                self.__log__("done.\n")
+        if not self.__copy_check_same__(package_path):
+            self.__copy__(package_path)
 
         # Import the temp Python file coming out of the package
         import temp
@@ -55,3 +56,21 @@ class LanguageManager:
                 log_file = open(lm_settings["logfile"], "w")
             log_file.write(val)
             log_file.close()
+
+    def __copy_check_same__(self, file):
+        with open(file, "r") as package:
+            with open("temp.py", "r") as file:
+                if package.read() == file.read():
+                    return True
+                return False
+
+    def __copy__(self, package):
+        with open(package, "r") as file:
+            with open(f"temp.py", "w") as temp_file:
+                # Log ONLY IF the "debug" value in the settings == True
+                self.__log__(f"--- Call to LanguageManager.get_key(self, {self.language_name_global}, "
+                             f"{self.key_global}) ---\nCopying file {package} to LanguageManager "
+                             f"root/temp.py\n")
+                # Copy the language package into the file temp.py in the root of LanguageManager
+                temp_file.write(file.read())
+                self.__log__("done.\n")
